6285b6cad725c063de47e62713ab3ba0c6a939f6
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2018-04-21 10:33:14 -0400

    Push of Angular website.

--- angular-final/src/app/app.module.ts
+++ angular-final/src/app/app.module.ts
@@ -1,18 +1,38 @@
+import { WeatherComponent } from './weather/weather.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DatePipe } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OurCustomersComponent } from './our-customers/our-customers.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { AppRoutingModule } from './/app-routing.module';
 
+import { WeatherService } from './weather.service';
+import { HomeComponent } from './home/home.component';
+import { HourlyComponent } from './hourly/hourly.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OurCustomersComponent,
+    AboutUsComponent,
+    OurServicesComponent,
+    WeatherComponent,
+    HomeComponent,
+    HourlyComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+      DatePipe,
+      WeatherService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

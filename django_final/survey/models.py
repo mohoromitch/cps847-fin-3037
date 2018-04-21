3dda9069d69f69a82201424f353c0d772a6de224
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2018-04-21 11:18:43 -0400

    Added migration steps to repo.

--- django_final/survey/models.py
+++ django_final/survey/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class SurveyQuestion(models.Model):
     respondent = models.CharField(max_length=100)
+    counter = models.IntegerField(default=5)
 
     def __str__(self):
         return self.question_text
